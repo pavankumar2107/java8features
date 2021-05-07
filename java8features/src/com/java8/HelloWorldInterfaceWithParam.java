707f8268130f086c55298e9518c386a5f8471a26
pavan.uskcorp <pavan.uskcorp@gmail.com>
2021-05-07 14:27:20 +0530

    lambda with param

---
+++ java8features/src/com/java8/HelloWorldInterfaceWithParam.java
@@ -0,0 +1,6 @@
+package com.java8;
+
+public interface HelloWorldInterfaceWithParam {
+
+	public String sayHelloWorld(String msg);
+}
--- java8features/src/com/java8/HelloWorldLambda.java
+++ java8features/src/com/java8/HelloWorldLambda.java
@@ -9,7 +9,9 @@ public class HelloWorldLambda {
 		System.out.println(helloWorldInterface.sayHelloWorld());
 
 		HelloWorldInterface helloWorldInterfaceWithoutReturn = () -> "hello World";
+		HelloWorldInterfaceWithParam helloWorldInterfaceWithParam = (str) -> str + " hello world ";
 		System.out.println(helloWorldInterfaceWithoutReturn.sayHelloWorld());
+		System.out.println(helloWorldInterfaceWithParam.sayHelloWorld("pavan"));
 	}
 
 }

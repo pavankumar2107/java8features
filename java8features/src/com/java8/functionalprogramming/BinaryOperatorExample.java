08ff9604c46e3593c7da1f047cf27179127af58a
pavan.uskcorp <pavan.uskcorp@gmail.com>
2021-05-20 12:20:49 +0530

    added BinaryOperatorExample

---
+++ java8features/src/com/java8/functionalprogramming/BinaryOperatorExample.java
@@ -0,0 +1,32 @@
+package com.java8.functionalprogramming;
+
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.IntBinaryOperator;
+import java.util.function.LongBinaryOperator;
+
+public class BinaryOperatorExample {
+
+	public static void main(String[] args) {
+
+		BinaryOperator<Integer> binaryOperator = (a, b) -> a + b;
+		System.out.println(binaryOperator.apply(2, 4));
+
+		Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
+		BinaryOperator<Integer> maxBi = BinaryOperator.maxBy(comparator);
+		System.out.println(maxBi.apply(7, 8));
+		BinaryOperator<Integer> minBi = BinaryOperator.minBy(comparator);
+		System.out.println(minBi.apply(7, 8));
+
+		IntBinaryOperator intBi = (a, b) -> a * b;
+		System.out.println(intBi.applyAsInt(2, 6));
+
+		LongBinaryOperator longBinaryOperator = (a, b) -> a * b;
+		System.out.println(longBinaryOperator.applyAsLong(4L, 6L));
+
+		DoubleBinaryOperator doubleBinaryOperator = (a, b) -> a * b;
+		System.out.println(doubleBinaryOperator.applyAsDouble(2.00, 5.00));
+	}
+
+}

9fbe048a3a2b152209464346562fd7fdeae7447b
pavan.uskcorp <pavan.uskcorp@gmail.com>
2021-05-24 17:28:21 +0530

    added FilterExample

---
+++ java8features/src/com/java8/streams/FilterExample.java
@@ -0,0 +1,18 @@
+package com.java8.streams;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.java8.functionalprogramming.Instructor;
+import com.java8.functionalprogramming.Instructors;
+
+public class FilterExample {
+
+	public static void main(String[] args) {
+		List<Instructor> list = Instructors.getAll().stream().filter(e -> e.getYearsOfExperience() > 10)
+				.sorted(Comparator.comparing(Instructor::getName)).collect(Collectors.toList());
+		System.out.println(list);
+	}
+
+}

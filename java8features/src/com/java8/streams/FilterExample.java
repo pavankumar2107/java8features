525af9e3d6e0129898ac21ea74bd1f227cc0d1f7
PRABHAKER <PRABHAKER@PRABAKAR>
2021-06-10 14:30:18 +0530

    test from thosiba

--- java8features/src/com/java8/streams/FilterExample.java
+++ java8features/src/com/java8/streams/FilterExample.java
@@ -12,6 +12,7 @@ public class FilterExample {
 	public static void main(String[] args) {
 		List<Instructor> list = Instructors.getAll().stream().filter(e -> e.getYearsOfExperience() > 10)
 				.sorted(Comparator.comparing(Instructor::getName)).collect(Collectors.toList());
+		
 		System.out.println(list);
 	}
 
